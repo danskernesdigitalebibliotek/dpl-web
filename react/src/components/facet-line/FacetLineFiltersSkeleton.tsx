reload/dpl-react

Add skeleton variety in facet line
To prevent layout jump on desktop

src/components/facet-line/FacetLineFiltersSkeleton.tsx
@@ -3,11 +3,20 @@ import { FC } from "react";
 
 const FacetLineFiltersSkeleton: FC = () => {
   return (
-    <div className="ssc mt-48">
-      <div className="ssc-head-line mb" />
-      <div className="ssc-head-line mb" />
-      <div className="ssc-head-line mb" />
-    </div>
+    <>
+      <div className="search-result-page__skeleton-facet-line--mobile">
+        <div className="ssc mt-48">
+          <div className="ssc-head-line mb" />
+          <div className="ssc-head-line mb" />
+          <div className="ssc-head-line mb" />
+        </div>
+      </div>
+      <div className="search-result-skeleton-facet-line--desktop">
+        <div className="ssc mt-48">
+          <div className="ssc-head-line mb" />
+        </div>
+      </div>
+    </>
   );
 };
 

danskernesdigitalebibliotek/dpl-web

react/src/components/facet-line/FacetLineFiltersSkeleton.tsx
@@ -3,11 +3,20 @@ import { FC } from "react";
 
 const FacetLineFiltersSkeleton: FC = () => {
   return (
-    <div className="ssc mt-48">
-      <div className="ssc-head-line mb" />
-      <div className="ssc-head-line mb" />
-      <div className="ssc-head-line mb" />
-    </div>
+    <>
+      <div className="search-result-page__skeleton-facet-line--mobile">
+        <div className="ssc mt-48">
+          <div className="ssc-head-line mb" />
+          <div className="ssc-head-line mb" />
+          <div className="ssc-head-line mb" />
+        </div>
+      </div>
+      <div className="search-result-page__skeleton-facet-line--desktop">
+        <div className="ssc mt-48">
+          <div className="ssc-head-line mb" />
+        </div>
+      </div>
+    </>
   );
 };
 