danskernesdigitalebibliotek/dpl-react

Refactor `getBlacklistedQueryArgs` to handle specific blacklist types
Since `useGetHoldings` serves multiple purposes, we need the flexibility to select a specific blacklist. For example, the message displaying the number of materials in the library should only use the `"availability"` blacklist.

I updated the code to ensure that the blacklist is always defined as `"availability" | "pickup" | "both"`, making the decision explicit and improving code readability.

src/components/find-on-shelf/FindOnShelfModalBody.tsx
@@ -54,7 +54,7 @@ const FindOnShelfModalBody: FC<FindOnShelfModalBodyProps> = ({
   );
   const { data, isLoading } = useGetHoldings({
     faustIds: faustIdArray,
-    useAvailabilityBlacklist: true,
+    blacklist: "both",
     config
   });
   const author = creatorsToString(flattenCreators(authors), t);

danskernesdigitalebibliotek/dpl-web

react/src/components/find-on-shelf/FindOnShelfModalBody.tsx
@@ -54,7 +54,7 @@ const FindOnShelfModalBody: FC<FindOnShelfModalBodyProps> = ({
   );
   const { data, isLoading } = useGetHoldings({
     faustIds: faustIdArray,
-    useAvailabilityBlacklist: true,
+    blacklist: "availability",
     config
   });
   const author = creatorsToString(flattenCreators(authors), t);