danskernesdigitalebibliotek/dpl-cms

Add work title to page title
Ref DDFSAL-207

web/modules/custom/dpl_fbi/src/Fbi.php
@@ -6,7 +6,7 @@ namespace Drupal\dpl_fbi;
 
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\dpl_library_agency\GeneralSettings;
+use Drupal\dpl_fbi\GraphQL\Operations\WorkInfo;
 use function Safe\preg_replace;
 
 /**
@@ -14,10 +14,19 @@ use function Safe\preg_replace;
  */
 class Fbi {
 
+  const FBI_PROFILE = 'next';
+
   /**
    * Configuration.
    */
   protected ImmutableConfig $config;
+
+  /**
+   * General settings from 'dpl_library_agency'.
+   *
+   * @todo This ought to be our own configuration, but await work on DDFNEXT-957.
+   */
+  protected ImmutableConfig $agencySettings;
 
   /**
    * Constructor.
@@ -25,10 +34,10 @@ class Fbi {
    * @todo Move the profile configuration to this module.
    */
   public function __construct(
-    protected GeneralSettings $agencySettings,
     ConfigFactoryInterface $configFactory,
   ) {
     $this->config = $configFactory->get('dpl_fbi.settings');
+    $this->agencySettings = $configFactory->get('dpl_library_agency.general_settings');
   }
 
   /**
@@ -38,7 +47,12 @@ class Fbi {
    *   Profile type to name mapping.
    */
   public function getProfiles(): array {
-    return $this->agencySettings->getFbiProfiles();
+    return $this->agencySettings->get('fbi_profiles') ?? [
+      FbiProfileType::DEFAULT->value => self::FBI_PROFILE,
+      FbiProfileType::LOCAL->value => self::FBI_PROFILE,
+      FbiProfileType::GLOBAL->value => self::FBI_PROFILE,
+    ];
+
   }
 
   /**
@@ -64,6 +78,33 @@ class Fbi {
     }
 
     return $urls;
+  }
+
+  /**
+   * Get the service URL for the given profile.
+   */
+  public function getServiceUrl(string $type): string {
+    $types = $this->getProfiles();
+
+    if (!isset($types[$type])) {
+      throw new \RuntimeException(sprintf('Unknown profile type %s', $type));
+    }
+
+    $profile = $types[$type];
+
+    return preg_replace('/\[profile\]/', $profile, $this->config->get('base_url'));
+  }
+
+  /**
+   * Get title of work.
+   *
+   * This should be replaced with a function that returns all the metadata the
+   * work page needs.
+   */
+  public function getWorkTitle(string $wid): string {
+    $info = WorkInfo::execute($wid);
+
+    return $info->errorFree()->data?->work->titles->full[0] ?? '';
   }
 
 }

danskernesdigitalebibliotek/dpl-web

cms/web/modules/custom/dpl_fbi/src/Fbi.php
@@ -6,7 +6,7 @@ namespace Drupal\dpl_fbi;
 
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\dpl_library_agency\GeneralSettings;
+use Drupal\dpl_fbi\GraphQL\Operations\WorkInfo;
 use function Safe\preg_replace;
 
 /**
@@ -14,10 +14,19 @@ use function Safe\preg_replace;
  */
 class Fbi {
 
+  const FBI_PROFILE = 'next';
+
   /**
    * Configuration.
    */
   protected ImmutableConfig $config;
+
+  /**
+   * General settings from 'dpl_library_agency'.
+   *
+   * @todo This ought to be our own configuration, but await work on DDFNEXT-957.
+   */
+  protected ImmutableConfig $agencySettings;
 
   /**
    * Constructor.
@@ -25,10 +34,10 @@ class Fbi {
    * @todo Move the profile configuration to this module.
    */
   public function __construct(
-    protected GeneralSettings $agencySettings,
     ConfigFactoryInterface $configFactory,
   ) {
     $this->config = $configFactory->get('dpl_fbi.settings');
+    $this->agencySettings = $configFactory->get('dpl_library_agency.general_settings');
   }
 
   /**
@@ -38,7 +47,12 @@ class Fbi {
    *   Profile type to name mapping.
    */
   public function getProfiles(): array {
-    return $this->agencySettings->getFbiProfiles();
+    return $this->agencySettings->get('fbi_profiles') ?? [
+      FbiProfileType::Default->value => self::FBI_PROFILE,
+      FbiProfileType::Local->value => self::FBI_PROFILE,
+      FbiProfileType::Global->value => self::FBI_PROFILE,
+    ];
+
   }
 
   /**
@@ -64,6 +78,33 @@ class Fbi {
     }
 
     return $urls;
+  }
+
+  /**
+   * Get the service URL for the given profile.
+   */
+  public function getServiceUrl(string $type): string {
+    $types = $this->getProfiles();
+
+    if (!isset($types[$type])) {
+      throw new \RuntimeException(sprintf('Unknown profile type %s', $type));
+    }
+
+    $profile = $types[$type];
+
+    return preg_replace('/\[profile\]/', $profile, $this->config->get('base_url'));
+  }
+
+  /**
+   * Get title of work.
+   *
+   * This should be replaced with a function that returns all the metadata the
+   * work page needs.
+   */
+  public function getWorkTitle(string $wid): string {
+    $info = WorkInfo::execute($wid);
+
+    return $info->errorFree()->data?->work->titles->full[0] ?? '';
   }
 
 }