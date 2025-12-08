c3766ac4225c093bef40d5f8da36026ffd977ee3
thomasgross <thomasgrossrasmussen@gmail.com>
2025-12-08 16:25:07 +0100

    fix: reorder import statements in test-runner.ts to pass tests

--- go/.storybook/test-runner.ts
+++ go/.storybook/test-runner.ts
@@ -1,5 +1,5 @@
-import { checkA11y, injectAxe } from "axe-playwright"
 import type { TestRunnerConfig } from "@storybook/test-runner"
+import { checkA11y, injectAxe } from "axe-playwright"
 
 const config: TestRunnerConfig = {
   async preVisit(page) {
