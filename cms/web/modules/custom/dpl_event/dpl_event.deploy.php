287bb1b89ddfb05ea32b325686a9ec8520153f1d
Benjamin Rasmussen <rasben@users.noreply.github.com>
2026-03-27 18:22:05 +0100

    Unset address data from events that only have countries set. DDF-159
    In a previous version, we set Denmark as the default country and made
    the individual address fields optional.
    This was an attempt to make the interface easier to use - but meant that
    the address field would never be empty - and break the logic that pulls
    address from the associated branch.
    
    We've reverted this change, so now Denmark is not set as default, and
    we've added a migrator to fix the "broken" events.
    A new ticket that solves the "nicer-interface" situation: DDF-201

--- cms/web/modules/custom/dpl_event/dpl_event.deploy.php
+++ cms/web/modules/custom/dpl_event/dpl_event.deploy.php
@@ -7,6 +7,7 @@
  * These get run AFTER config-import.
  */
 
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
 
 /**
@@ -86,4 +87,73 @@ function dpl_event_deploy_migrate_all_day_events(): string {
   $count = count($instances);
 
   return "Updated $count all-day events, to set 00:00 - 23:59 time.";
+}
+
+/**
+ * Unset address data from events that only have countries set.
+ *
+ * In a previous version, we set Denmark as the default country and made the
+ * individual address fields optional.
+ * This was an attempt to make the interface easier to use - but meant that the
+ * address field would never be empty - and break the logic that pulls address
+ * from the associated branch.
+ *
+ * We'll look up any eventinstances/eventseries that only have a country set as
+ * an address and unset the value completely.
+ *
+ * Used by
+ * - dpl_event_deploy_migrate_country_eventseries()
+ * - dpl_event_deploy_migrate_country_eventinstances()
+ */
+function _dpl_event_unset_country_only_addresses(EntityStorageInterface $storage): string {
+  $field_name = 'field_event_address';
+  $query = $storage->getQuery();
+
+  // NULL is saved in different ways, in series/instances, for some reason.
+  $condition_group = $query->orConditionGroup()
+    ->condition("$field_name.address_line1", NULL, 'IS NULL')
+    ->condition("$field_name.address_line1", '');
+
+  $ids = $query
+    ->condition("$field_name.country_code", NULL, 'IS NOT NULL')
+    ->condition($condition_group)
+    // We do not need an access check, as it's a migrator.
+    ->accessCheck(FALSE)
+    ->execute();
+
+  if (empty($ids)) {
+    return 'Found no relevant events to update.';
+  }
+
+  $count = count($ids);
+
+  /** @var \Drupal\recurring_events\Entity\EventSeries[]|\Drupal\recurring_events\Entity\EventInstance[] $entities */
+  $entities = $storage->loadMultiple($ids);
+
+  foreach ($entities as $entity) {
+    $entity->set($field_name, []);
+    $entity->save();
+  }
+
+  return "Updated $count events, removing country-only address.";
+}
+
+/**
+ * Unset address data from eventseries that only have countries set.
+ *
+ * @see _dpl_event_unset_country_only_addresses()
+ */
+function dpl_event_deploy_migrate_country_eventseries(): string {
+  $storage = \Drupal::entityTypeManager()->getStorage('eventseries');
+  return _dpl_event_unset_country_only_addresses($storage);
+}
+
+/**
+ * Unset address data from eventinstances that only have countries set.
+ *
+ * @see _dpl_event_unset_country_only_addresses()
+ */
+function dpl_event_deploy_migrate_country_eventinstances(): string {
+  $storage = \Drupal::entityTypeManager()->getStorage('eventinstance');
+  return _dpl_event_unset_country_only_addresses($storage);
 }
