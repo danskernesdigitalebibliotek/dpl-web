76c092b7c040dd253acda2493b8be729ff4ee942
thomasgross <thomasgrossrasmussen@gmail.com>
2026-04-09 12:31:12 +0200

    chore(lagoon): add environment variables for node service and improve comments in start script

--- go/lagoon/start.sh
+++ go/lagoon/start.sh
@@ -1,10 +1,12 @@
 # Set the environment variables.
 # These ones are varying from environment to environment.
 
-# For PR environments, use varnish domain for CMS communication
+# For PR environments, ensure CMS communication goes through varnish
+# These should already be set at build time via cms/lagoon/node.dockerfile,
+# but we set them here as well for runtime consistency
 if [[ "$LAGOON_ENVIRONMENT" =~ ^pr- ]]; then
   # PR environments: CMS is at varnish.pr-XXX.project.domain
-  export NEXT_PUBLIC_APP_URL="https://${LAGOON_DOMAIN}"
+  # NEXT_PUBLIC_APP_URL should already be set correctly at build time
   export NEXT_PUBLIC_DPL_CMS_HOSTNAME="varnish.${LAGOON_ENVIRONMENT}.${LAGOON_PROJECT}.dplplat02.dpl.reload.dk"
   export NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_DPL_CMS="https://varnish.${LAGOON_ENVIRONMENT}.${LAGOON_PROJECT}.dplplat02.dpl.reload.dk/graphql"
 else
