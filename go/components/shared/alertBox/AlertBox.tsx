d28d3f53c5fc6b686e90f696e7c48b49050651e0
thomasgross <thomasgrossrasmussen@gmail.com>
2026-04-27 16:57:48 +0200

    fix: improve color contrast for success AlertBox to meet WCAG AA

--- go/components/shared/alertBox/AlertBox.tsx
+++ go/components/shared/alertBox/AlertBox.tsx
@@ -18,7 +18,7 @@ export default function AlertBox({
       className={cn("rounded-base mx-auto flex items-center gap-4 p-4", {
         "bg-error-red-100 text-error-red-400": variant === "error",
         "bg-warning-orange-100 text-warning-orange-400": variant === "warning",
-        "bg-success-green-100 text-success-green-400": variant === "success",
+        "bg-success-green-100 text-success-green-500": variant === "success",
       })}>
       <Icon className="h-5 min-h-5 w-5 min-w-5" name={icon} />
       <p className="text-typo-link">{message}</p>
