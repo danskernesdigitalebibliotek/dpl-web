fa582598794a41f0a556833b80a14cf6b216d50b
Thomas Fini Hansen <xen@xen.dk>
2026-05-11 14:45:16 +0200

    Don't sync content slider
    Apparently it's used for app content, which shouldn't be synced to
    libraries. Secondly BNF specifically want to keep the app content on
    the page and sync the rest of the content to library sites (as opposed
    to maintaining two pages).
    
    So this makes the content slider disappear in the sync.

--- cms/web/modules/custom/bnf/tests/src/Unit/Mapper/ParagraphContentSliderMapperTest.php
+++ cms/web/modules/custom/bnf/tests/src/Unit/Mapper/ParagraphContentSliderMapperTest.php
@@ -4,25 +4,15 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\bnf\Unit\Mapper;
 
-use Drupal\bnf\BnfMapperManager;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\ContentReferences\NodePage;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\ParagraphContentSlider;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\UnderlinedTitle\Text;
 use Drupal\bnf\Plugin\bnf_mapper\ParagraphContentSliderMapper;
-use Drupal\paragraphs\Entity\Paragraph;
-use Prophecy\Prophecy\ObjectProphecy;
 
 /**
  * Testing the content_slider mapper.
  */
 class ParagraphContentSliderMapperTest extends BnfMapperImportReferencePluginBaseTest {
-
-  /**
-   * Entity prophecy.
-   *
-   * @var \Prophecy\Prophecy\ObjectProphecy<\Drupal\Core\Entity\EntityBase>
-   */
-  protected ObjectProphecy $entityProphecy;
 
   /**
    * {@inheritDoc}
@@ -34,36 +24,20 @@ class ParagraphContentSliderMapperTest extends BnfMapperImportReferencePluginBas
       [],
       '',
       [],
-      $this->entityTypeManager->reveal(),
-      $this->importContextStack->reveal(),
-      $this->importer->reveal(),
-      $this->prophesize(BnfMapperManager::class)->reveal(),
     );
-
-    $this->entityProphecy = $this->prophesize(Paragraph::class);
   }
 
   /**
-   * Test mapping a content slider paragraph.
+   * Test that we're not mapping anything.
    */
-  public function testReferenceImport(): void {
+  public function testThatContentSliderIsntImported(): void {
     $contentReferences = [];
-    $expectedContentReferences = [];
 
     for ($i = 1; $i <= 3; $i++) {
       $uuid = "content-uuid-$i";
 
-      $this->prophesizeImportedNode((string) $i, $uuid);
-
-      $nodeReference = NodePage::make(
-        id: $uuid,
-      );
+      $nodeReference = NodePage::make(id: $uuid);
       $contentReferences[] = $nodeReference;
-
-      $expectedContentReferences[] = [
-        'target_id' => (string) $i,
-        'target_type' => 'node',
-      ];
     }
 
     $graphqlElement = ParagraphContentSlider::make(
@@ -75,17 +49,7 @@ class ParagraphContentSliderMapperTest extends BnfMapperImportReferencePluginBas
       contentReferences: $contentReferences,
     );
 
-    $this->paragraphStorage->create([
-      'type' => 'content_slider',
-      'field_title' => '',
-      'field_underlined_title' => [
-        'value' => 'Underlined title value',
-        'format' => 'basic_html',
-      ],
-      'field_content_references' => $expectedContentReferences,
-    ])->willReturn($this->entityProphecy)->shouldBeCalled();
-
-    $this->assertsame($this->mapper->map($graphqlElement), $this->entityProphecy->reveal());
+    $this->assertsame($this->mapper->map($graphqlElement), []);
   }
 
 }
