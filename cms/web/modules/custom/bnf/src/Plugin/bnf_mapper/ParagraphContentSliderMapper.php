67baf53672268e667b025e5b785cd8b711af6c74
Thomas Fini Hansen <xen@xen.dk>
2026-05-11 14:45:16 +0200

    Don't sync content slider
    Apparently it's used for app content, which shouldn't be synced to
    libraries. Secondly BNF specifically want to keep the app content on
    the page and sync the rest of the content to library sites (as opposed
    to maintaining two pages).
    
    So this makes the content slider disappear in the sync.

--- cms/web/modules/custom/bnf/src/Plugin/bnf_mapper/ParagraphContentSliderMapper.php
+++ cms/web/modules/custom/bnf/src/Plugin/bnf_mapper/ParagraphContentSliderMapper.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace Drupal\bnf\Plugin\bnf_mapper;
 
 use Drupal\bnf\Attribute\BnfMapper;
-use Drupal\bnf\BnfMapperManager;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\ParagraphContentSlider;
-
-use Drupal\bnf\Services\BnfImporter;
-use Drupal\bnf\Services\ImportContextStack;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Spawnia\Sailor\ObjectLike;
 
 /**
@@ -19,47 +14,14 @@ use Spawnia\Sailor\ObjectLike;
 #[BnfMapper(
   id: ParagraphContentSlider::class,
 )]
-class ParagraphContentSliderMapper extends BnfMapperImportReferencePluginBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(
-    array $configuration,
-    string $pluginId,
-    array $pluginDefinition,
-    EntityTypeManagerInterface $entityTypeManager,
-    protected ImportContextStack $importContext,
-    protected BnfImporter $importer,
-    protected BnfMapperManager $mapper,
-  ) {
-    parent::__construct($configuration, $pluginId, $pluginDefinition, $entityTypeManager, $importContext, $importer);
-  }
+class ParagraphContentSliderMapper extends BnfMapperPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function map(ObjectLike $object): mixed {
-    if (!($object instanceof ParagraphContentSlider)) {
-      throw new \RuntimeException('Wrong class handed to mapper');
-    }
-
-    $references = [];
-
-    if (isset($object->contentReferences) && !empty($object->contentReferences)) {
-      $references = $this->mapEntityReferences(array_map(fn ($ref) => $ref->id, $object->contentReferences));
-    }
-
-    return $this->entityTypeManager->getStorage('paragraph')->create([
-      'type' => 'content_slider',
-      'field_title' => $object->title ?? NULL,
-      'field_underlined_title' => [
-        'value' => $object->underlinedTitle?->value ?? '',
-        'format' => $object->underlinedTitle?->format ?? '',
-      ],
-      'field_content_references' => $references,
-    ]);
-
+    // Don't map this paragraph.
+    return [];
   }
 
 }
