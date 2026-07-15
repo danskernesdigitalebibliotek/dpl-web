bf61b018833f473f2d4401eb3cc39f7a7e8c05c2
thomasgross <thomasgrossrasmussen@gmail.com>
2026-07-15 12:11:45 +0200

    style(go): flatten info box sections

--- go/components/shared/infoBox/InfoBoxDetails.stories.tsx
+++ go/components/shared/infoBox/InfoBoxDetails.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
   decorators: [
     Story => {
       return (
-        <div className="content-container my-grid-gap-2 lg:my-grid-gap-half flex-row flex-wrap">
+        <div className="content-container flex-row flex-wrap">
           <Story />
         </div>
       )
