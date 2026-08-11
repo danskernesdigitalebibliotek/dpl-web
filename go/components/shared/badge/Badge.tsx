4ec235ab5dea0e17c84ce04c2cc66274abfffaec
Lisa Bianca Henning Søndergaard <lbs@reload.dk>
2026-08-11 11:49:58 +0200

    Fix badge position in search list
    Badge was positioned behind book cover on mobile screens. A z-index: 1 was introduced to position the badge in front.
    Ticket: DDF-458

--- go/components/shared/badge/Badge.tsx
+++ go/components/shared/badge/Badge.tsx
@@ -1,11 +1,11 @@
-import { type VariantProps, cva } from "class-variance-authority"
-import * as React from "react"
+import { type VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
 
-import { cn } from "@/lib/helpers/helper.cn"
+import { cn } from "@/lib/helpers/helper.cn";
 
 const badgeVariants = cva(
   `inline-flex items-center rounded-full px-2 py-1 text-typo-tag-sm font-semibold transition-colors
-  text-background`,
+  text-background z-1`,
   {
     variants: {
       variant: {
@@ -19,13 +19,13 @@ const badgeVariants = cva(
       variant: "default",
     },
   }
-)
+);
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
+  extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> { }
 
 function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
