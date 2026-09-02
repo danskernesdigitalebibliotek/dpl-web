b0529d832539dcea90b8a7ce1aea4e95c2f5bebe
Tomas Lieberkind <tomas@reload.dk>
2026-09-02 23:38:26 +0200

    refactor(Cover): split data fetching from rendering
    Creates a dedicated `StaticCover` component which is purely responsible
    for rendering a cover image and has no knowledge of how the cover image
    is fetched. It's more or less a copy-paste version of the "render part"
    of the Cover component.
    
    The Cover component now uses the StaticCover component internally for
    rendering.

---
+++ react/src/components/cover/static-cover.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import LinkNoStyle, { LinkNoStyleProps } from "../atoms/links/LinkNoStyle";
+import CoverImage, { CoverImageProps } from "./cover-image";
+import clsx from "clsx";
+import { DisplaySize } from "./cover.types";
+
+type StaticCoverProps = {
+  src: string;
+  animate?: boolean;
+  tint?: Tint;
+  alt?: string;
+  shadow?: CoverImageProps["shadow"];
+  onImageLoaded?: CoverImageProps["onImageLoaded"];
+  hasImageLoaded?: boolean;
+  displaySize: DisplaySize;
+  linkProps?: {
+    url?: LinkNoStyleProps["url"];
+    ariaLabelledBy?: LinkNoStyleProps["ariaLabelledBy"];
+    trackClick?: LinkNoStyleProps["trackClick"];
+  };
+};
+
+export const StaticCover = ({
+  linkProps,
+  displaySize,
+  src,
+  alt,
+  onImageLoaded = noop,
+  hasImageLoaded = true,
+  shadow,
+  animate = false,
+  tint = "default"
+}: StaticCoverProps) => {
+  const { url, ariaLabelledBy, trackClick } = linkProps ?? {};
+
+  const classes = {
+    wrapper: clsx(
+      "cover",
+      `cover--size-${displaySize}`,
+      `cover--aspect-${displaySize}`,
+      hasImageLoaded || tintClassNames[tint]
+    )
+  };
+
+  if (url) {
+    return (
+      <LinkNoStyle
+        className={classes.wrapper}
+        url={url}
+        ariaLabelledBy={ariaLabelledBy}
+        isHiddenFromScreenReaders={!alt}
+        trackClick={trackClick}
+      >
+        <CoverImage
+          onImageLoaded={onImageLoaded}
+          src={src}
+          altText={alt}
+          animate={animate}
+          shadow={shadow}
+        />
+      </LinkNoStyle>
+    );
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <CoverImage
+        onImageLoaded={onImageLoaded}
+        src={src}
+        altText={alt}
+        animate={animate}
+        shadow={shadow}
+      />
+    </div>
+  );
+};
+
+type Tint = "20" | "40" | "80" | "100" | "120" | "default";
+
+type TintClassNames = {
+  [key in Tint]: string;
+};
+
+const tintClassNames: TintClassNames = {
+  default: "bg-identity-tint-120",
+  "20": "bg-identity-tint-20",
+  "40": "bg-identity-tint-40",
+  "80": "bg-identity-tint-80",
+  "100": "bg-identity-tint-100",
+  "120": "bg-identity-tint-120"
+};
+
+const noop = () => {};
+
+StaticCover.displayName = "StaticCover";
