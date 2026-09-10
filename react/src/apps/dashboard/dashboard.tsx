47dda46dfa533e77efa99500bdcbf5d414084768
Tomas Lieberkind <tomas@reload.dk>
2026-09-10 14:43:18 +0200

    refactor: move fetching of loans and reservations out of Dashboard component

--- react/src/apps/dashboard/dashboard.tsx
+++ react/src/apps/dashboard/dashboard.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import DashboardFees from "./dashboard-fees/dashboard-fees";
 import DashboardNotificationList from "./dashboard-notification-list/dashboard-notification-list";
 import { useText } from "../../core/utils/text";
+import useLoans from "../../core/utils/useLoans";
+import useReservations from "../../core/utils/useReservations";
 
 interface DashboardProps {
   pageSize: number;
@@ -10,14 +12,21 @@ interface DashboardProps {
 const DashBoard: FC<DashboardProps> = ({ pageSize }) => {
   const t = useText();
 
+  const loans = useLoans();
+  const reservations = useReservations();
+
   return (
     <div className="dashboard-page">
       <h1 className="text-header-h1 mt-32 mb-64" data-cy="dashboard-header">
         {t("yourProfileText")}
       </h1>
-
       <DashboardFees />
-      <DashboardNotificationList columns pageSize={pageSize} />
+      <DashboardNotificationList
+        columns
+        pageSize={pageSize}
+        loans={loans}
+        reservations={reservations}
+      />
     </div>
   );
 };
