097337e5d786d15452460b9541774bc155beda13
Lisa Bianca Henning Søndergaard <lbs@reload.dk>
2026-09-15 14:27:06 +0200

    fix linting error (prettier test in CI)

--- go/components/shared/infoBox/InfoBoxItem.tsx
+++ go/components/shared/infoBox/InfoBoxItem.tsx
@@ -12,7 +12,7 @@ const InfoBoxItem = ({ term, children, classname }: InfoBoxItemProps) => {
   return (
     <>
       <hr className="not-first:mt-7" aria-hidden="true" />
-      <dt className="text-typo-caption opacity-70 mt-2">{term}</dt>
+      <dt className="text-typo-caption mt-2 opacity-70">{term}</dt>
       <dd className={cn("text-typo-heading-5 mt-6", classname)}>{children}</dd>
     </>
   )
