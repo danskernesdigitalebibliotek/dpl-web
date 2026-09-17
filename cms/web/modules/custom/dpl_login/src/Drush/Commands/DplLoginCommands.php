0cf927466e6c03035f230d5aad332276966dfb87
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2026-09-17 09:10:42 +0200

    Also expire unregistered tokens in dpl_login:token-expire
    UserTokens::getCurrent() prefers the unregistered token, so expiring only
    the registered one left a live token in play.

--- cms/web/modules/custom/dpl_login/src/Drush/Commands/DplLoginCommands.php
+++ cms/web/modules/custom/dpl_login/src/Drush/Commands/DplLoginCommands.php
@@ -5,6 +5,7 @@ namespace Drupal\dpl_login\Drush\Commands;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\KeyValueStore\KeyValueExpirableFactory;
 use Drupal\dpl_login\RegisteredUserTokensProvider;
+use Drupal\dpl_login\UnregisteredUserTokensProvider;
 use Drush\Attributes\Command;
 use Drush\Attributes\Argument;
 use Drush\Attributes\Usage;
@@ -31,7 +32,11 @@ class DplLoginCommands extends DrushCommands {
   }
 
   /**
-   * Forcefully expire a user library token.
+   * Forcefully expire a user token.
+   *
+   * Covers both the registered and the unregistered user token, as a user can
+   * hold either - and UserTokens::getCurrent() prefers the unregistered one,
+   * so leaving it alone would keep handing out a live token.
    *
    * Only alters Drupal idea of when the token expires, it doesn't change the
    * token at adgangsplatformen.
@@ -40,29 +45,59 @@ class DplLoginCommands extends DrushCommands {
    */
   #[Command(name: 'dpl_login:token-expire')]
   #[Argument(name: 'uid', description: 'User uid.')]
-  #[Usage(name: 'dpl_login:token-expire 10', description: 'Expire token for user 10.')]
+  #[Usage(name: 'dpl_login:token-expire 10', description: 'Expire tokens for user 10.')]
   public function token(string $uid): void {
+    $providers = [
+      'Unregistered user token' => UnregisteredUserTokensProvider::class,
+      'Registered user token' => RegisteredUserTokensProvider::class,
+    ];
+
+    $expired = FALSE;
+    foreach ($providers as $label => $provider) {
+      $expired = $this->expireToken($provider, $uid, $label) || $expired;
+    }
+
+    if (!$expired) {
+      $this->io()->error(dt('Could not find any tokens for user.'));
+    }
+  }
+
+  /**
+   * Expire the token held by a single token provider, if there is one.
+   *
+   * @param string $provider
+   *   Class name of the token provider owning the temp store.
+   * @param string $uid
+   *   User uid.
+   * @param string $label
+   *   Human readable name of the token, for the command output.
+   *
+   * @return bool
+   *   TRUE if a token was found and expired.
+   */
+  protected function expireToken(string $provider, string $uid, string $label): bool {
     // As the tokens are stored in private temp store, we have to dig it out
     // ourselves.
-    $collection = 'tempstore.private.' . RegisteredUserTokensProvider::class;
+    $collection = 'tempstore.private.' . $provider;
     $key = $uid . ':access_token';
     $token = $this->storageFactory->get($collection)->get($key);
 
     if (!$token) {
-      $this->io()->error(dt('Could not find token for user.'));
-      return;
+      return FALSE;
     }
 
     $expire = new DateTimeImmutable('@' . $token->data->expire);
-    $this->io()->info('Existing expire: ' . $expire->format('c'));
+    $this->io()->info($label . ' - existing expire: ' . $expire->format('c'));
 
     $newExpire = new DateTimeImmutable('@' . $this->datetime->getCurrentTime());
 
-    $this->io()->info('New expire: ' . $newExpire->format('c'));
+    $this->io()->info($label . ' - new expire: ' . $newExpire->format('c'));
 
     $token->data->expire = $newExpire->getTimestamp();
 
     $this->storageFactory->get($collection)->set($key, $token);
+
+    return TRUE;
   }
 
 }
